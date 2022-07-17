3ba73a678dd82c481736c9624d9af4c61b435ae1
AidanMolina <63667680+AidanMolina@users.noreply.github.com>
2022-07-17 10:40:34 -0500

    Add files via upload

---
+++ AirlineTestCase.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+/*
+public class AirlineTestClass {
+
+	@Test
+	public void testCount() {
+		Airline a = new Airline(String name, String loginPassword);
+		a.addFlights(new Flight ourFlight(ArrayList<FlightStop> route, int speed, int fuel, int numberOfRows, int numberOfSeatsInEachRow, int id));
+		a.removeFlight(ourFlight);
+		a.addReview(new Review blahblah(int 10, String "bad review", UserAccount reviewer, Flight flight));
+		a.giveMiles(10);
+		assertEquals("bad review" , a.viewReviews())
+	}
+
+
+}*/
